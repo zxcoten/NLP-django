9c0ddadbb706aba11d7485d684cc059a22e5f4f8
Azamat <s.ecc.216@gmail.com>
2022-04-25 18:48:55 +0600

    added frontend first steps

--- kaznlpdjango/main/views.py
+++ kaznlpdjango/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return render(request, )
 # Create your views here.
+
+def tokenz(request):
+    return HttpResponse("<table>hello</table>")
